3fb2ed733926434b8823bab7d73f3ee9ca8d7d55
Mark <markmekosh@gmail.com>
2020-02-11 12:01:17 -0600

    Update

--- Homework2/Sympletcic_Hamiltonian_Kepler.jl
+++ Homework2/Sympletcic_Hamiltonian_Kepler.jl
@@ -14,7 +14,7 @@ q2 = 0.0
 
 t = 0.0
 dt = 0.01
-N = 1000000
+N = 10000
 
 Time = [t]
 p1_values = [p1]
@@ -36,16 +36,16 @@ for i in 1:N
     push!(p2_values, p2)
     push!(q1_values, q1)
     push!(q2_values, q2)
-    if i%5 == 0
-        plot(q1_values,q2_values,title="Symplectic Euler, e = 0.6",xlabel="q1",ylabel="q2",xlims=(-4,4),ylims=(-4,4))
-        gui()
-    end
+    #if i%5 == 0
+    #    plot(q1_values,q2_values,title="Symplectic Euler, e = 0.6",xlabel="q1",ylabel="q2",xlims=(-4,4),ylims=(-4,4))
+    #    gui()
+    #end
     t += dt
     push!(Time, t)
 end
 
-#H_values = H.(q1_values, q2_values, p1_values, p2_values)
-#display(plot(q1_values, q2_values, title = "Symplectic Explicit Euler, e = 0.6",xlabel="q1", ylabel="q2"))
-#display(plot(p1_values, p2_values, title = "Symplectic Explicit Euler, e = 0.6",xlabel="p1", ylabel="p2"))
-#display(plot(Time, H_values, title="Energy of the system",xlabel="Time",ylabel="Energy",ylims=(-1,0)))
+H_values = H.(q1_values, q2_values, p1_values, p2_values)
+display(plot(q1_values, q2_values, title = "Symplectic Explicit Euler, e = 0.6",xlabel="q1", ylabel="q2"))
+display(plot(p1_values, p2_values, title = "Symplectic Explicit Euler, e = 0.6",xlabel="p1", ylabel="p2"))
+display(plot(Time, H_values, title="Energy of the system",xlabel="Time",ylabel="Energy",ylims=(-1,0)))
 end
