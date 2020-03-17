87868ba7ea8696e52a4a765a2fec8fe8ce4be4ae
Mark Mekosh <markmekosh@gmail.com>
2020-03-17 09:11:50 -0500

    Simulation working

--- Homework4/Molecular_Dynamics2.jl
+++ Homework4/Molecular_Dynamics2.jl
@@ -3,54 +3,61 @@ using Plots
 const N = 100
 const L = 30
 const g = 9.8
-const dt = 0.0001
+const dt = 0.001
 const dt2 = dt^2
 const time_steps = 5000
 const m = 1.0
 const σ = 1
 const ϵ = 1
+const eps = 1e-8
 
 
 function Initialize(N)
-    x = repeat(collect(10.0:2:28), 10)
-    y = repeat(collect(10.0:2:28), inner=10)
+    global m, g
+    x = repeat(collect(10.5:19.5), 10)
+    y = repeat(collect(10.5:19.5), inner=10)
     vx = zeros(N)
     vy = zeros(N)
     ax = zeros(N)
     ay = zeros(N)
-    return x, y, vx, vy, ax, ay
+    ax1 = zero(N)
+    ay1 = zeros(N)
+    return x, y, vx, vy, ax, ay, ax1, ay1
 end
 
 function Force(dx, dy)
-    global σ, ϵ, g
-    a = σ/ϵ^2
+    global σ, ϵ, g, eps
     r2 = (dx^2 + dy^2)
-    r2inv = 1/r2
-    r2inv3 = r2inv^3
-    f = 24 * a * r2inv3 * (2*r2inv3 - 1) * r2inv
+    if r2 < eps
+        r2 = eps
+    end
+    r2inv = 1.0/r2
+    er2 = ϵ^2 * r2inv
+    a = er2^3
+    f = 24.0 * σ * r2inv * a * (2.0*a - 1.0)
     fx = f*dx
     fy = f*dy
     return fx, fy
 end
 
-function Verlet!(x, y, vx, vy, ax, ay)
+function Verlet!(x, y, vx, vy, ax, ay, ax1, ay1)
     global L, dt, dt2
     for i in 1:N
         xnew = x[i] + vx[i]*dt + 0.5*ax[i]*dt2
         ynew = y[i] + vy[i]*dt + 0.5*ay[i]*dt2
 
-        vx[i] = vx[i] + 0.5*ax[i]*dt
-        vy[i] = vy[i] + 0.5*ay[i]*dt
-
         x[i], vx[i] = xBoundary!(xnew, vx[i], L)
         y[i], vy[i] = yBoundary!(ynew, vy[i], L)
 
+    #    vx[i] = vx[i] + 0.5*ax[i]*dt
+    #    vy[i] = vy[i] + 0.5*ay[i]*dt
+
     end
 
-    ax, ay = Accel!(x, y, ax, ay)
+    ax1, ay1 = Accel!(x, y, ax, ay)
     for i in 1:N
-        vx[i] += 0.5*ax[i]*dt
-        vy[i] += 0.5*ay[i]*dt
+        vx[i] += 0.5*(ax[i] + ax1[i])*dt
+        vy[i] += 0.5*(ay[i] + ay1[i])*dt
     end
     return x, y
 end
@@ -74,35 +81,38 @@ function yBoundary!(pos, v, L)
     return pos, v
 end
 
-
 function Plot(x, y, L)
-    scatter(x, y, xlims=(-L,2L), ylims=(0,2L), legend=false)
+    scatter(x, y, xlims=(0,L), ylims=(0,L), legend=false)
     gui()
 end
 
 function Accel!(x, y, ax, ay)
-    global g
-    for i in 1:N-1
+    global g, m
+    ax .= 0.0
+    ay .= -m*g
+
+    for i in 1:N
         for j in i+1:N
             dx = x[i] - x[j]
             dy = y[i] - y[j]
             fx, fy = Force(dx, dy)
             ax[i] += fx
-            ay[i] += fy - g
+            ay[i] += fy
 
             ax[j] -= fx
-            ay[j] -= fy + g
+            ay[j] -= fy
         end
     end
         return ax, ay
 end
 
 let
-    x, y, vx, vy, ax, ay = Initialize(N)
+    x, y, vx, vy, ax, ay, ax1, ay1 = Initialize(N)
     for i in 1:time_steps
-        x, y = Verlet!(x, y, vx, vy, ax, ay)
+        ax, ay = Accel!(x, y, ax, ay)
+        x, y = Verlet!(x, y, vx, vy, ax, ay, ax1, ay1)
         Plot(x, y, L)
-        sleep(0.05)
+        #sleep(0.05)
     end
 end
 
